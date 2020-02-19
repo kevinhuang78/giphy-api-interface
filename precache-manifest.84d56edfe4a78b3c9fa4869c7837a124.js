d68ed534a8a85c6abed4a60fd2e08835a832f5c8
kevinhuang78 <kevin_huang_iim@yahoo.com>
2020-02-19 09:54:31 +0100

    Updates

---
+++ precache-manifest.84d56edfe4a78b3c9fa4869c7837a124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c6a84fea30f860846b04275623d3ba",
+    "url": "/giphy-api-interface/index.html"
+  },
+  {
+    "revision": "843a67d63efc02716d9d",
+    "url": "/giphy-api-interface/static/css/2.e48f1a7a.chunk.css"
+  },
+  {
+    "revision": "d5de521e631aa28998c5",
+    "url": "/giphy-api-interface/static/css/main.f65345ac.chunk.css"
+  },
+  {
+    "revision": "843a67d63efc02716d9d",
+    "url": "/giphy-api-interface/static/js/2.ad5d1506.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/giphy-api-interface/static/js/2.ad5d1506.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5de521e631aa28998c5",
+    "url": "/giphy-api-interface/static/js/main.d1ab7095.chunk.js"
+  },
+  {
+    "revision": "39024efe9f8bc064bd6e",
+    "url": "/giphy-api-interface/static/js/runtime-main.654da9da.js"
+  }
+]);
