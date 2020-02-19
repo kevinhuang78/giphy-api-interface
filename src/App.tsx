f2035df86510fa9969b589ca4487fe20cbe30eff
kevinhuang78 <kevin_huang_iim@yahoo.com>
2020-02-19 10:23:51 +0100

    feat: change routing for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import "./sass/index.scss";
 import configureStore from "./reducers/configureStore";
 import Homepage from "./screens/Homepage";
@@ -14,8 +14,9 @@ const App: React.FC = () => (
     <Router>
         <Provider store={store}>
             <Switch>
-                <Route exact path="/" component={Homepage} />
-                <Route exact path="/search" component={SearchScreen} />
+                <Redirect exact from="/" to="/giphy-api-interface" />
+                <Route exact path="/giphy-api-interface" component={Homepage} />
+                <Route exact path="/giphy-api-interface/search" component={SearchScreen} />
                 <Route component={Error404} />
             </Switch>
             {process.env.NODE_ENV === "development" && <DevTools />}
